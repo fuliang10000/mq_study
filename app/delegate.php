22037bfc70a41f48413cf8af8f8462f8c25000cd
fuliang <632249982@qq.com>
2021-08-27 10:21:59 +0800

    提交

--- app/delegate.php
+++ app/delegate.php
@@ -4,10 +4,25 @@
  */
 namespace app;
 
-use common\delegate\MyObject;
+use common\delegate\NewPlayList;
+use common\delegate\PlayList;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$myObject = new MyObject();
-$myObject->setDelegateType('A')->createDelegateObject()->runDelegateAction();
-$myObject->setDelegateType('B')->createDelegateObject()->runDelegateAction();
+$externalRetrievedType = $_SERVER['argv'][1] ?? 'pls';
+//$playList = new PlayList();
+//$playList->addSong('/Users/fuliang/code/fuliang10000/mq_study/brr.mp3', 'Brr');
+//$playList->addSong('/Users/fuliang/code/fuliang10000/mq_study/goodbye.mp3', 'Goodbye');
+//
+//if ($externalRetrievedType == 'pls') {
+//    $playListContent = $playList->getPLS();
+//} else {
+//    $playListContent = $playList->getM3U();
+//}
+//
+//echo $playListContent;
+
+$newPlayList = new NewPlayList($externalRetrievedType);
+$newPlayList->addSong('/Users/fuliang/code/fuliang10000/mq_study/brr.mp3', 'Brr');
+$newPlayList->addSong('/Users/fuliang/code/fuliang10000/mq_study/goodbye.mp3', 'Goodbye');
+echo $newPlayList->getPlayList();
